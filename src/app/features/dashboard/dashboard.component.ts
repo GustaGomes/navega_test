a3c06821529aad6043c1f451d23af251753bdd85
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2025-05-28 16:08:47 -0300

    config dashboard

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartOptions, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.less']
+  styleUrls: ['./dashboard.component.less'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  pieChartType: ChartType = 'doughnut';
 
+  pieChartData: ChartData<'doughnut', number[], string | string[]> = {
+    labels: ['Contribuição mensal', 'Contribuição voluntária'],
+    datasets: [
+      {
+        data: [500000, 500000],
+        backgroundColor: ['#F44336', '#3F51B5'],
+      },
+    ],
+  };
+
+  pieChartOptions: ChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'bottom',
+      },
+    },
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
